716fef37d15771f10106c403e98e750ef9275ad1
Sheila Schlickmann <sheila31sl@hotmail.com>
2021-11-15 19:23:42 -0300

    Lista de Cursos: Tratamento de Erros e MatDialog

--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
     MatTableModule,
     MatCardModule,
     MatToolbarModule,
-    MatProgressSpinnerModule],
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule],
 })
 export class AppMaterialModule { }
